3247555ca1581fd0ac8adc7865bd1b02e02bdd8e
Amr Khalil <44541932+amr-khalil@users.noreply.github.com>
2020-12-06 03:55:58 +0100

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app.add_url_rule('/base', 'base', views.base)
 app.add_url_rule('/', 'index', views.index)
 app.add_url_rule('/faceapp', 'faceapp', views.faceapp)
 app.add_url_rule('/face_recognition', 'face_recognition', views.face_recognition, methods=['GET', 'POST'])
-app.add_url_rule('/add_face', 'add_face', views.add_face, methods=['GET', 'POST'])
+app.add_url_rule('/management', 'management', views.management, methods=['GET', 'POST'])
 
 # run
 if __name__=='__main__':
