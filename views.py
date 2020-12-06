c256244a25785cbb534a55d385c5f52b639ee0e1
Amr Khalil <44541932+amr-khalil@users.noreply.github.com>
2020-12-06 03:56:48 +0100

    Update views.py

--- views.py
+++ views.py
@@ -1,7 +1,7 @@
 from flask import render_template, redirect, request, url_for
 import os
 from PIL import Image
-from api import detect_faces, detect_labels, get_img_metadata, add_face_to_collection,remove_face_from_collection
+from api import detect_faces, detect_labels, get_img_metadata, add_face_to_collection,remove_face_from_collection, show_known_people, is_one_face
 import random
 
 UPLOAD_FOLDER = './static/uploads/'
@@ -52,29 +52,52 @@ def face_recognition():
 	return render_template('face_recognition.html', fileupload=False)
 
 
-def add_face():
+def management():
+
+	# List Faces
+	faces_lsit = show_known_people()
+
 	if request.method == 'POST':
+		
+		# Add Face
 		if request.form['submit'] == "submit_add":
 			f = request.files['image']
 			fname = request.form.get('fname')
 			lname = request.form.get('lname')
+			group = request.form.get('group')
+
 			fname = fname.capitalize()
 			lname = lname.capitalize()
+			group = group.capitalize()
+
 			filename = f.filename
-			path = os.path.join('./static/known_pepole', "{}_{}_{}".format(fname,lname,filename))
+			path = os.path.join('./static/known_people', "{}_{}_{}.jpg".format(fname,lname,group))
 			f.save(path)
-			add_face_to_collection(path,fname,lname)
-			return render_template('add_face.html',face_add = True, fname=fname,face_remove=False, lname=lname)
+			one_face = is_one_face(path)
+			if one_face == False:
+				os.remove(path)
+			else:	
+				add_face_to_collection(path,fname,lname,group)
+				return render_template('management.html',face_add = True, fname=fname,face_remove=False, lname=lname,
+				 alert=False, faces_lsit=faces_lsit, one_face=one_face)
 
+			return render_template('management.html',face_add = True, fname=fname,face_remove=False, lname=lname,
+				 alert=False, faces_lsit=faces_lsit, one_face=False)
+
+		# Remove Face
 		if request.form['submit'] == 'submit_remove':
-			fname = request.form.get('fname')
-			lname = request.form.get('lname')
-			fname = fname.capitalize()
-			lname = lname.capitalize()
-			face_remove = remove_face_from_collection(fname, lname)
-			return render_template('add_face.html',face_add = False,face_remove=face_remove, fname=fname, lname=lname)
+			img_path = request.form.get('img_path')
+			os.remove("./static/known_people/{}".format(img_path))
+			img_id = img_path.split('.')
+			img_id = img_id[0]
+			face_remove = remove_face_from_collection(img_id)
+			if face_remove == False and request.form['submit']:
+				alert = True
+				return render_template('management.html',face_add = False,face_remove=face_remove,alert=False, faces_lsit=faces_lsit)
 
-	return render_template('add_face.html',face_add = False, face_remove=False)
+			return render_template('management.html',face_add = False,face_remove=face_remove,alert=False, faces_lsit=faces_lsit)
+
+	return render_template('management.html',face_add = False, face_remove=False, alert=False, faces_lsit=faces_lsit, one_face=False)
 
 
 
